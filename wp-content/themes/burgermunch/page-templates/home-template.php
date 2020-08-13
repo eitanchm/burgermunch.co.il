813ef32eeddb9cfd7972ef8f400a95aa18387c3c
Eitan Chmelevsky <eitan4562@gmail.com>
2020-08-13 12:53:11 +0300

    Card section done

--- wp-content/themes/burgermunch/page-templates/home-template.php
+++ wp-content/themes/burgermunch/page-templates/home-template.php
@@ -21,18 +21,46 @@ get_header();
 </section>
 
 <section class="home-card-section row">
-    <div class="card brown-card col-sm4">
-        <i class="fas fa-hamburger card-icon"></i>
-    </div>
-    <div class="card yellow-card col-sm4">
-        <i class="fas fa-hamburger card-icon"></i>
-    </div>
-    <div class="card brown-card col-sm4">
-        <i class="fas fa-hamburger card-icon"></i>
-    </div>
-    <div class="card yellow-card col-sm4">
-        <i class="fas fa-hamburger card-icon"></i>
-    </div>
+    <a href="/תפריט" class="card-link">
+        <div class="card brown-card col-sm4">
+            <i class="fa fa-check-circle card-icon"></i>
+            <div class="text-wrap">
+                <h3 class="card-text">תפריט</h3>
+                <div class="div-line"></div>
+                <h3 class="card-text">MENU</h3>
+            </div>
+        </div>
+    </a>
+    <a href="/תפריט" class="card-link">
+        <div class="card yellow-card col-sm4">
+            <i class="fa fa-check-circle card-icon"></i>
+            <div class="text-wrap">
+                <h3 class="card-text">משלוחים</h3>
+                <div class="div-line"></div>
+                <h3 class="card-text">DELIVERY</h3>
+            </div>
+        </div>
+    </a>
+    <a href="/תפריט" class="card-link">
+        <div class="card brown-card col-sm4">
+            <i class="fa fa-check-circle card-icon"></i>
+            <div class="text-wrap">
+                <h3 class="card-text">מאנץ' חדש</h3>
+                <div class="div-line"></div>
+                <h3 class="card-text">NEW MUNCH</h3>
+            </div>
+        </div>
+    </a>
+    <a href="/תפריט" class="card-link">
+        <div class="card yellow-card col-sm4">
+            <i class="fa fa-check-circle card-icon"></i>
+            <div class="text-wrap">
+                <h3 class="card-text">מועדון</h3>
+                <div class="div-line"></div>
+                <h3 class="card-text">CLUB</h3>
+            </div>
+        </div>
+    </a>
 </section>
 
 <?php
