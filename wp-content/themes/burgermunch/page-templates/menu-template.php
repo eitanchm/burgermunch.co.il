128d9189ba4bc62107f7bba26eb74b104df721d1
Eitan Chmelevsky <eitan4562@gmail.com>
2020-08-24 17:40:38 +0300

    Restaurant menu layout done.

--- wp-content/themes/burgermunch/page-templates/menu-template.php
+++ wp-content/themes/burgermunch/page-templates/menu-template.php
@@ -14,15 +14,46 @@ defined('ABSPATH') || exit;
 get_header();
 ?>
 
+<!-- Start page title section -->
+
 <section class="page-title-section">
     <h2 class="page-title">תפריט</h2>
     <img src="<?php echo get_template_directory_uri(); ?>/img/burger-background.png"
         alt="small-repeated-hamburger-background" class="burger-background" />
 </section>
 
+<!-- End page title section -->
+
+<!-- Start delivery number section -->
+
 <section class="delivery-number-section">
     <h2 class="delivery-number">למשלוחים חייגו>> <span class="red-text">*2395</span> </h2>
 </section>
+
+<!-- End delivery number section -->
+
+<!-- Start menu section -->
+
+<section class="restaurant-menu-section">
+    <div class="menu-row">
+        <div class="yellow-menu-circle menu-tab"></div>
+        <div class="filler-red-circle left-filler-circle"></div>
+    </div>
+    <div class="menu-row">
+        <div class="filler-red-circle right-filler-circle"></div>
+        <div class="brown-menu-circle menu-tab"></div>
+    </div>
+    <div class="menu-row">
+        <div class="yellow-menu-circle menu-tab"></div>
+        <div class="filler-red-circle left-filler-circle"></div>
+    </div>
+    <div class="menu-row">
+        <div class="filler-red-circle right-filler-circle"></div>
+        <div class="brown-menu-circle menu-tab"></div>
+    </div>
+</section>
+
+<!-- End menu section -->
 
 <?php
 get_footer();
